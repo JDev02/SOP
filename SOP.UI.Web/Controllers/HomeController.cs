c2d992e75ca2b7c52ba6db0d77e16e7b34438bc9
john <john02dev@gmail.com>
2014-04-02 13:45:58 -0300

    added testproject

--- SOP.UI.Web/Controllers/HomeController.cs
+++ SOP.UI.Web/Controllers/HomeController.cs
@@ -3,11 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using SOP.Business.Services;
 
 namespace SOP.UI.Web.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly IAddressService _addressService;
+
+        public HomeController(IAddressService addressService)
+        {
+            _addressService = addressService;
+        }
+
         public ActionResult Index()
         {
             ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
