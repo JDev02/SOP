928b5f3082f048f04b8e856ee83884d8c4c7c55c
john <john02dev@gmail.com>
2014-04-02 13:47:24 -0300

    add test ptoject

---
+++ SOP.UI.Test/Web/HomeTest.cs
@@ -0,0 +1,28 @@
+﻿using System.Web.Mvc;
+using NUnit.Framework;
+using SOP.Business.Services;
+using SOP.Data.Repositories;
+using SOP.UI.Web.Controllers;
+
+namespace SOP.UI.Test.Web
+{
+    [TestFixture]
+    public class HomeTest
+    {
+        [Test]
+        public void SiHomeDaComoRespuestaUnViewBag()
+        {
+            // Arrange
+            var addressRep = new sqlAddressRepository(@"C:\hola.txt");
+            var adreesService = new AddressService(addressRep);
+            HomeController controller = new HomeController(adreesService);
+
+            // Act
+            ViewResult result = controller.Index() as ViewResult;
+
+            // Assert
+            ViewDataDictionary viewData = result.ViewData;
+            Assert.AreEqual("Modify this template to jump-start your ASP.NET MVC application.", viewData["Message"]);
+        }
+    }
+}
---
+++ SOP.UI.Web/ControllerFactory/PoorManDIControllerFactory.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Web.Mvc;
+using SOP.Business.Services;
+using SOP.Data.Repositories;
+using SOP.UI.Web.Controllers;
+
+namespace SOP.UI.Web.ControllerFactory
+{
+    public class PoorManDIControllerFactory : DefaultControllerFactory
+    {
+        protected override IController GetControllerInstance(System.Web.Routing.RequestContext requestContext, Type controllerType)
+        {
+            //var customerRepository = new CustomerRepository(@"C:\hola.txt");
+            //var customerService = new CustomerService(customerRepository);
+
+            //return controllerType == typeof(HomeController) ?
+            //new HomeController(customerService) : base.GetControllerInstance(requestContext, controllerType);
+            var txtaddressRespository = new AddressRepository(@"C:\hola.txt");
+            var sqladdressRespository = new sqlAddressRepository(@"C:\hola.txt");
+            var addressService = new AddressService(sqladdressRespository);
+            return controllerType == typeof(HomeController) ?
+            new HomeController(addressService) : base.GetControllerInstance(requestContext, controllerType);
+
+        } 
+    }
+}
